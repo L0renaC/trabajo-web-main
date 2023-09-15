1f28b0b09958e9a3c702a724607fc8614fd351f2
L0renaC <lorenacadvid@gmail.com>
2023-09-15 14:24:36 -0500

    feat: trajo de loging

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-card
+    class="mx-auto"
+    width="400"
+  >
+  <div>
+    <center>
+      <h1>Login</h1>
+      <p>Esta es una aplicación que permite gestionar las tareas de los estudiantes</p>
+        <v-sheet width="300" class="mx-auto">
+    <v-card-item>
+    <v-form fast-fail @submit.prevent>
+
+      <v-text-field
+        v-model="Email"
+        label="Email"
+        :rules="Email"
+      ></v-text-field>
+
+       <v-text-field
+        label="Password"
+        type="Password"
+        hint="Password"
+      ></v-text-field>
+      
+
+      <v-btn href="/tareas" type="submit" block class="mt-2">Ingresar</v-btn>
+    </v-form>
+    </v-card-item>
+  </v-sheet>
+
+<br>
+<br>
+      <NuxtLink to="/tareas">
+        Click para iniciar (Con nuxtLink)
+      </NuxtLink>
+    </center>
+
+  </div>
+  </v-card>
+</template>
+<script>
+definePageMeta({
+  layout: "blank",
+});
+</script>
